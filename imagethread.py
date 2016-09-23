00ac41b779e3a64472aec1bbc6315dae12a09cc2
Cade Brown <brown.cade@gmail.com>
2016-09-22 22:43:35 -0400

    Updated with frame

--- imagethread.py
+++ imagethread.py
@@ -5,8 +5,6 @@ import cmath
 from math import *
 #Runs with set paramaters and stores in arr
 def run(DIMENSIONS, CENTER, ZOOM, ITER, PATTERN, FRAME_NUM, FRAME_MAX, func=None):
-    sys.stdout.write("\r %" + str(int(100 * (FRAME_NUM + 1) / FRAME_MAX)) + " Done  ")
-    sys.stdout.flush()
     arr = numpy.zeros((DIMENSIONS[1], DIMENSIONS[0], 3), dtype=numpy.uint8)
     x = CENTER[0] - 1.0 / (ZOOM)
     px = 0
@@ -32,4 +30,6 @@ def run(DIMENSIONS, CENTER, ZOOM, ITER, PATTERN, FRAME_NUM, FRAME_MAX, func=None
             py += 1
         x += d
         px += 1
+    sys.stdout.write("\r %" + str(int(100 * (FRAME_NUM + 1) / FRAME_MAX)) + " Done  ")
+    sys.stdout.flush()
     return (arr, FRAME_NUM)
--- run.py
+++ run.py
@@ -11,6 +11,7 @@ import os
 from os import listdir
 from os.path import isfile, join
 import shutil
+from math import *
 
 import imagethread
 
@@ -20,14 +21,14 @@ parser = argparse.ArgumentParser(description='Generate fractal images with Fract
 parser.add_argument('-d', '--dimensions', default=[800, 800], nargs=2, type=int, help='Dimensions of the image. Default is: 800 800')
 parser.add_argument('-c', '--center', default=[0, 0], nargs=2, type=float, help='Center of the image. Default is 0 0')
 parser.add_argument('-z', '--zoom', default=1, help='Scale. Default is 1')
-parser.add_argument('-i', '--iterations', default=12, type=int,help='Iterations: default is 12')
+parser.add_argument('-i', '--iterations', default='12',help='Iterations: default is 12')
 parser.add_argument('-p', '--pattern', default='MOCHA', help='Color pattern. Default is MOCHA. Check wiki for more')
 parser.add_argument('-a', '--animate', action='store_true', default=False, help="Use this flag to use an animated gif")
 parser.add_argument('-s', '--seconds', default=4, type=int, help='Seconds, only matters if using -a. Default: 2')
 parser.add_argument('-fps', '--framespersecond', default=8, type=int, help='Frames per second, only matters if using -a. Default: 12')
 parser.add_argument('-zps', '--zoompersecond', default=2, type=float, help='Zoom per second, only matters if using -a. Default: 3.5')
-parser.add_argument('-o', '--output', default="out.png", help='Output file. Don\'t put `.png` or any other extension. Default: out')
-parser.add_argument('-t', '--threads', default=multiprocessing.cpu_count(), help='Number of thread. Default: Number of CPU cores')
+parser.add_argument('-o', '--output', help='Output file. Make sure to use correct output extension')
+parser.add_argument('-t', '--threads', default=multiprocessing.cpu_count() - 1, help='Number of thread. Default: Number of CPU cores')
 parser.add_argument('-f', '--function', default=None, help='Function to compute. Default is mandelbrot set')
 args = parser.parse_args()
 
@@ -41,6 +42,12 @@ ZOOM = float(args.zoom)
 ITER = args.iterations
 #Color pattern
 PATTERN = args.pattern
+if not args.output:
+    if (args.animate):
+        args.output = "out.gif"
+    else:
+        args.output = "out.png"
+
 
 FI = args.output.split(".")[0]
 FMT = args.output.split(".")[1]
@@ -48,6 +55,7 @@ FIN = FI + "." + FMT
 
 #If they want an animated gif, run in lots of threads to maximize resources
 if (args.animate):
+    BASE_ZOOM = ZOOM + 0.0
     #How many frames?
     FRAMES = int(args.framespersecond * args.seconds)
     ARR_ARR = [None] * FRAMES
@@ -56,23 +64,26 @@ if (args.animate):
         ARR_ARR[res[1]] = res[0]
     #This limits the number of executed threads
     pool = Pool(THREADS)
-    ADJ_ZOOM = float(ZOOM)
-    ADJ_ITER = ITER
     #loop through and create threads to run, only calling threads at a time
-    for i in range(0, FRAMES, THREADS):
+    for FRAME in range(0, FRAMES):
+        TIME = (FRAME + 0.0) / args.framespersecond
+        #Adjust the ZOOM
+        ZOOM = BASE_ZOOM * (args.zoompersecond ** TIME)
+        LGZOOM = log(ZOOM)
+        #Evaluate iteration
+        ADJ_ITER = eval(ITER)
         #Create threads
-        for j in range(0, THREADS):
-            #Start a thread in the pool.
-            pool.apply_async(imagethread.run, args=(copy.copy(DIMENSIONS), copy.copy(CENTER), copy.copy(ADJ_ZOOM), copy.copy(int(ADJ_ITER)), copy.copy(PATTERN), copy.copy(i + j), copy.copy(FRAMES), copy.copy(args.function)), callback=threadCallback)
-            #Adjust the ZOOM
-            ADJ_ZOOM = ADJ_ZOOM * (args.zoompersecond ** (1.0 / args.framespersecond))
+        pool.apply_async(imagethread.run, args=(copy.copy(DIMENSIONS), copy.copy(CENTER), copy.copy(ZOOM), copy.copy(int(ADJ_ITER)), copy.copy(PATTERN), copy.copy(FRAME), copy.copy(FRAMES), copy.copy(args.function)), callback=threadCallback)
     #Close and wait
     pool.close()
     pool.join()
     #Save to file
     imageio.mimsave(FIN, ARR_ARR, format=FMT, fps=args.framespersecond)
 else:
+    if not args.output:
+        args.output = "out.png"
     #Run in this thread, it will be too much hassle to use threads for one image.
-    res = imagethread.run(DIMENSIONS, CENTER, ZOOM, ITER, PATTERN, 0, 1, args.function)
+    FRAME = 0
+    res = imagethread.run(DIMENSIONS, CENTER, ZOOM, eval(ITER), PATTERN, 0, 1, args.function)
     imageio.imsave(FIN, res[0], format=FMT)
 print("\nFile saved: " + FIN)
