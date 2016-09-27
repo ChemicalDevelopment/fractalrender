2a9e3b88b802d034a85ea2bac4ebdde04cca63a2
Cade Brown <brown.cade@gmail.com>
2016-09-27 10:00:23 -0400

    Added cache reader/writer

--- mandelbrot.c
+++ mandelbrot.c
@@ -177,7 +177,7 @@ void returnIterArray(int frame) {
 		data_png[i] = .75 + .25 * ((8 * (maxIter - data[i] + 256)) % 256) / 256.0;
 	}	
 	char fn[120];
-	snprintf(fn, sizeof fn, "./output/tmp/file%d.png", frame);
+	snprintf(fn, sizeof fn, "./tmp/file%d.png", frame);
 	writeImage(fn, width, height, data_png, "Mandelbrot");
 }
 
@@ -203,7 +203,7 @@ int main(int argc, char *argv[])
 	ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
 	
 	//Get device ID's'
-	ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_CPU, 1, &device_id, &ret_num_devices);
+	ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1, &device_id, &ret_num_devices);
 	
 	/* Create OpenCL context */
 	context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
--- run.py
+++ run.py
@@ -16,7 +16,6 @@ import time
 
 import imagethread
 
-
 #Set args
 parser = argparse.ArgumentParser(description='Generate fractal images with FractalRenderPy')
 parser.add_argument('-d', '--dimensions', default=[800, 800], nargs=2, type=int, help='Dimensions of the image. Default is: 800 800')
@@ -25,6 +24,7 @@ parser.add_argument('-z', '--zoom', default=1.0, help='Scale. Default is 1')
 parser.add_argument('-i', '--iterations', default='12',help='Iterations: default is 12')
 parser.add_argument('-p', '--pattern', default='MOCHA', help='Color pattern. Default is MOCHA. Check wiki for more')
 parser.add_argument('-a', '--animate', action='store_true', default=False, help="Use this flag to use an animated gif")
+parser.add_argument('-com', '--combine', action='store_true', default=False, help="Use this flag to combine currently computed frames.")
 parser.add_argument('-cl', '--opencl', action='store_true', default=False, help="Use this flag to use OpenCL")
 parser.add_argument('-s', '--seconds', default=4, type=int, help='Seconds, only matters if using -a. Default: 2')
 parser.add_argument('-fps', '--framespersecond', default=8, type=int, help='Frames per second, only matters if using -a. Default: 12')
@@ -33,6 +33,7 @@ parser.add_argument('-o', '--output', help='Output file. Make sure to use correc
 parser.add_argument('-t', '--threads', default=multiprocessing.cpu_count() - 1, help='Number of thread. Default: Number of CPU cores')
 parser.add_argument('-f', '--function', default=None, help='Function to compute. Default is mandelbrot set')
 args = parser.parse_args()
+
 
 #Pixel dimensions
 DIMENSIONS = tuple(args.dimensions)
@@ -44,70 +45,70 @@ ZOOM = float(args.zoom)
 ITER = args.iterations
 #Color pattern
 PATTERN = args.pattern
+#Num frames
+FRAMES = int(args.framespersecond * args.seconds)
+#Base zoom
+BASE_ZOOM = ZOOM + 0.0
+THREADS = int(args.threads)
+
+
+#Set if output is empty
 if not args.output:
     if (args.animate):
-        args.output = "out.gif"
+        args.output = "out.mp4"
     else:
         args.output = "out.png"
-
 
 FI = args.output.split(".")[0]
 FMT = args.output.split(".")[1]
 FIN = FI + "." + FMT
+
 try:
     os.remove(FIN)
 except:
     pass
 
+WRITER = imageio.get_writer(FIN, fps=int(args.framespersecond))
+
+def combineFrames():
+    for i in range(0, FRAMES):
+        WRITER.append_data(imageio.imread("./tmp/file" + str(i) + ".png"))
+    WRITER.close()
+
 start_time = time.time()
 
-#If they want an animated gif, run in lots of threads to maximize resources
-if (args.animate):
-    if args.opencl:
-        FRAMES = int(args.framespersecond * args.seconds)
-        #./mandelbrot.o seconds frames width height maxIter centerX centerY zoom zoomps
-        commandA = ["./mandelbrot.o", args.seconds, FRAMES, DIMENSIONS[0], DIMENSIONS[1], ITER, CENTER[0], CENTER[1], ZOOM, args.zoompersecond]
-        commandA = map(str, commandA)
-        command = " ".join(commandA)
-        print command
-        process = subprocess.Popen(command, shell=True)
-        process.wait()
-        arr = []
-        for i in range(0, FRAMES):
-            arr.append(imageio.imread("./output/tmp/file" + str(i) + ".png"))
-        imageio.mimsave(FIN, arr, format=FMT, fps=args.framespersecond)
-    else:
-        BASE_ZOOM = ZOOM + 0.0
-        #How many frames?
-        FRAMES = int(args.framespersecond * args.seconds)
-        ARR_ARR = [None] * FRAMES
-        THREADS = int(args.threads)
-        def threadCallback(res):
-            ARR_ARR[res[1]] = res[0]
-        #This limits the number of executed threads
-        pool = Pool(THREADS)
-        #loop through and create threads to run, only calling threads at a time
-        for FRAME in range(0, FRAMES):
-            TIME = (FRAME + 0.0) / args.framespersecond
-            #Adjust the ZOOM
-            ZOOM = BASE_ZOOM * (args.zoompersecond ** TIME)
-            LGZOOM = log(ZOOM)
-            #Evaluate iteration
-            ADJ_ITER = eval(ITER)
-            #Create threads
-            pool.apply_async(imagethread.run, args=(copy.copy(DIMENSIONS), copy.copy(CENTER), copy.copy(ZOOM), copy.copy(int(ADJ_ITER)), copy.copy(PATTERN), copy.copy(FRAME), copy.copy(FRAMES), copy.copy(args.function)), callback=threadCallback)
-        #Close and wait
-        pool.close()
-        pool.join()
-        #Save to file
-        imageio.mimsave(FIN, ARR_ARR, format=FMT, fps=args.framespersecond)
+if (args.combine):
+    combineFrames()
 else:
-    if not args.output:
-        args.output = "out.png"
-    #Run in this thread, it will be too much hassle to use threads for one image.
-    FRAME = 0
-    res = imagethread.run(DIMENSIONS, CENTER, ZOOM, eval(ITER), PATTERN, 0, 1, args.function)
-    imageio.imsave(FIN, res[0], format=FMT)
+    if args.animate:
+        if args.opencl:
+            commandA = ["./mandelbrot.o", args.seconds, FRAMES, DIMENSIONS[0], DIMENSIONS[1], ITER, CENTER[0], CENTER[1], ZOOM, args.zoompersecond]
+            commandA = map(str, commandA)
+            command = " ".join(commandA)
+            print command
+            process = subprocess.Popen(command, shell=True)
+            process.wait()
+        else:
+            def threadCallback(res):
+                imageio.imsave("./output/tmp/file" + str(res[1]) + ".png", res[0])
+            #This limits the number of executed threads
+            pool = Pool(THREADS)
+            #loop through and create threads to run, only calling threads at a time
+            for FRAME in range(0, FRAMES):
+                TIME = (FRAME + 0.0) / args.framespersecond
+                #Adjust the ZOOM
+                ZOOM = BASE_ZOOM * (args.zoompersecond ** TIME)
+                #Evaluate iteration
+                ADJ_ITER = eval(ITER)
+                #Create threads
+                pool.apply_async(imagethread.run, args=(copy.copy(DIMENSIONS), copy.copy(CENTER), copy.copy(ZOOM), copy.copy(int(ADJ_ITER)), copy.copy(PATTERN), copy.copy(FRAME), copy.copy(FRAMES), copy.copy(args.function)), callback=threadCallback)
+            #Close and wait
+            pool.close()
+            pool.join()
+        combineFrames()
+    else:
+        imagethread.run(DIMENSIONS, CENTER, ZOOM, ITER, PATTERN, 0)
+
 end_time = time.time()
 print("\nFile saved: " + FIN)
 time_took = end_time - start_time
